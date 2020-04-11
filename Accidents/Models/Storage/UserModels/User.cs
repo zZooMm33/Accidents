6217a0cc646671526e81bc78115c7690e43410e6
Ярослав Алексеев <mmmoonnn@mail.ru>
2020-04-11 16:03:19 +0300

    Добавлены все стр, осталось в теории только сделать статистику

--- Accidents/Models/Storage/UserModels/User.cs
+++ Accidents/Models/Storage/UserModels/User.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Accidents.Models.Storage.UserModel
 {
     public class User
-    {
+    {        
         public int Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
