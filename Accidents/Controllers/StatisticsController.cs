9918515b81103c588e865a8e131473097a8ada3b
zzoomm <mmmoonnn@mail.ru>
2020-04-21 13:42:58 +0300

    Небольшая статистика для профессий. Так же был изменен excel файл.

---
+++ Accidents/Controllers/StatisticsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Accidents.Models;
+using Accidents.Models.Storage;
+using System.Data.Entity;
+using PagedList;
+
+namespace Accidents.Controllers
+{    
+    public class StatisticsController : Controller
+    {        
+        public ActionResult Profession(int id, int? page, string Search, string SearchBy)
+        {
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                List<Accident> accidents;
+                if (Search != null && Search.Length > 0)
+                {
+                    if (SearchBy == "Title") accidents = db.Accidents.Include(p => p.Profession).Include(d => d.Danger).Include(s => s.SourceDanger).Where(a => a.Title.IndexOf(Search) != -1).Where(p => p.ProfessionId == id).ToList();
+                    else if (SearchBy == "Description") accidents = db.Accidents.Include(p => p.Profession).Include(d => d.Danger).Include(s => s.SourceDanger).Where(a => a.Description.IndexOf(Search) != -1).Where(p => p.ProfessionId == id).ToList();
+                    else if (SearchBy == "Profession") accidents = db.Accidents.Include(p => p.Profession).Include(d => d.Danger).Include(s => s.SourceDanger).Where(a => a.Profession.Title.IndexOf(Search) != -1).Where(p => p.ProfessionId == id).ToList();
+                    else if (SearchBy == "Danger") accidents = db.Accidents.Include(p => p.Profession).Include(d => d.Danger).Include(s => s.SourceDanger).Where(a => a.Danger.Title.IndexOf(Search) != -1).Where(p => p.ProfessionId == id).ToList();
+                    else accidents = db.Accidents.Include(p => p.Profession).Include(d => d.Danger).Include(s => s.SourceDanger).Where(a => a.SourceDanger.Title.IndexOf(Search) != -1).Where(p => p.ProfessionId == id).ToList();
+                }
+                else accidents = db.Accidents.Include(p => p.Profession).Include(d => d.Danger).Include(s => s.SourceDanger).Where(p => p.ProfessionId == id).ToList();
+
+                ViewBag.StatisticsTitle = db.Professions.Where(p => p.Id == id).FirstOrDefault().Title;
+
+                return View(accidents.ToPagedList((page ?? 1), 5));
+            }
+        }
+
+        public ActionResult Danger(int id, int? page)
+        {
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                List<Accident> dangerAccidents = db.Accidents.Include(p => p.Profession).Include(d => d.Danger).Include(s => s.SourceDanger).Where(p => p.DangerId == id).ToList();
+                return View(dangerAccidents.ToPagedList((page ?? 1), 10));
+            }
+        }
+
+        public ActionResult SourceDanger(int id, int? page)
+        {
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                List<Accident> sourceDangerAccidents = db.Accidents.Include(p => p.Profession).Include(d => d.Danger).Include(s => s.SourceDanger).Where(p => p.SourceDangerId == id).ToList();
+                return View(sourceDangerAccidents.ToPagedList((page ?? 1), 10));
+            }
+        }
+    }
+}
